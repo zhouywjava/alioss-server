f81afdde7f1ad193a9ccbdffae11c3a7e63ed5cd
zhouyiweijava <67976661@qq.com>
2018-01-02 17:21:03 +0800

    springmvc多视图的配置

--- src/main/java/com/alioss/controller/HomeController.java
+++ src/main/java/com/alioss/controller/HomeController.java
@@ -10,12 +10,18 @@ import org.springframework.web.bind.annotation.RequestMapping;
  * @Date: 2017/12/13
  */
 @Controller
-@RequestMapping("/alioss")
+@RequestMapping("/home")
 public class HomeController {
 
     @GetMapping("/index")
     public String index(){
         System.out.println("Helloworld");
         return "index";
+    }
+
+    @GetMapping("/hello")
+    public String hello(){
+        System.out.println("Helloworld");
+        return "hello";
     }
 }
