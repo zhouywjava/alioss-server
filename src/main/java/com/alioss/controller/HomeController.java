ab2c08fb6d3ee58bfd3b0d0667dd91827a2d69c3
zhouyiweijava <67976661@qq.com>
2018-01-05 10:15:28 +0800

    springmvc前后端分离并且配置springmvc的json转换器

--- src/main/java/com/alioss/controller/HomeController.java
+++ src/main/java/com/alioss/controller/HomeController.java
@@ -2,6 +2,7 @@ package com.alioss.controller;
 
 import com.alioss.model.Person;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -22,6 +23,7 @@ public class HomeController {
     }
 
     @ResponseBody
+    @CrossOrigin(origins = "http://localhost:8080")
     @GetMapping("/hello")
     public Person hello(){
         Person person = new Person();
